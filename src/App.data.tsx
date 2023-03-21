8fa1300d7465b174f68d545bfaa95809a3325f89
Duy Nhat <nguyennhat.ggt@gmail.com>
2023-03-22 00:48:08 +0700

    update: splash

--- src/App.data.tsx
+++ src/App.data.tsx
@@ -1,9 +1,14 @@
 import { RouteObject } from 'react-router-dom';
 import Splash from './components/splash/Splash';
+import Profile from './pages/profile/profile';
 
 export const routeObjects: RouteObject[] = [
   {
     path: '',
     element: <Splash canProfileClick={true} />,
+  },
+  {
+    path: '/profile',
+    element: <Profile />,
   },
 ];
--- src/components/splash/Splash.tsx
+++ src/components/splash/Splash.tsx
@@ -1,17 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
 interface SplashProps {
   canProfileClick?: boolean;
 }
 
 const Splash = ({ canProfileClick }: SplashProps) => {
+  const navigate = useNavigate();
+
   const handleProfileClick = () => {
     if (!canProfileClick) return;
+    navigate('/profile');
   };
   return (
     <div
-      className={`text-[black] text-4xl ${canProfileClick ? 'cursor-pointer' : 'cursor-default'}`}
+      className={`relative text-[black] select-none p-4 ${
+        canProfileClick ? 'cursor-pointer' : 'cursor-default'
+      }`}
       onClick={handleProfileClick}
     >
-      John Nguyen
+      <p data-replace="Explored ►" className="splash-title text-[black] text-4xl">
+        <span>John Nguyen</span>
+      </p>
     </div>
   );
 };
